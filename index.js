7ca42e6ff1316d202b3e0e8556c8e23c3460a023
Roodiha <rodiha040@gmail.com>
2025-01-07 10:21:53 +0100

    create, update and delete

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ let students = [
 // CREATE ITEM
 app.post('/students', (req, res) => {
     const { name, age, gender } = req.body
+    if(!name || !age || !gender){
+        res.status(400).json({message:'Name, age and gender are required'})
+    }
     const newStudent = {
         id : students.length + 1,
         name,
@@ -29,8 +32,57 @@ app.post('/students', (req, res) => {
         gender
     }
     students.push(newStudent)
+    console.log(students)
     res.status(201).json(newStudent)
 })
+
+// GET ALL STUDENTS
+app.get('/students', (req, res) => {
+    res.status(200).json(students)
+})
+
+// GET SINGLE STUDENT
+app.get('/students/:id', (req, res) => {
+    const studentId = parseInt(req.params.id)
+    const student = students.find(s => s.id === studentId )
+    
+    if(!student){
+        res.status(400).json({message:'Student not found'})
+    }
+    res.status(200).json(student)
+})
+
+// UPDATE SINGLE STUDENT
+app.put('/students/:id', (req, res) => {
+    const studentId = parseInt(req.params.id)
+    const{name, age, gender} = req.body
+    const student = students.find( s => s.id === studentId)
+
+    if(!student){
+        res.status(400).json({message:'Student  not found'})
+    }
+
+    if (!name || !age || !gender){
+        res.status(400).json({message:'Name, age and gender are required'})
+    }
+
+    students[student] = {id:studentId, name, age, gender}
+    res.status(200).json(students[student])
+})
+
+//DELETE STUDENT
+app.delete('/students/:id', (req, res) => {
+    const studentId = parseInt(req.params.id)
+    const student = students.find(s => s.int === studentId)
+
+    if(!student){
+        res.status(400).json({message: 'Student not found'})
+    }
+
+    students.splice(student, 1)
+    res.status(200).json({message:'Student deleted successfully'})
+})
+
 
 app.listen(PORT, () => {
     console.log(`server running on http://localhost:${PORT}`)
